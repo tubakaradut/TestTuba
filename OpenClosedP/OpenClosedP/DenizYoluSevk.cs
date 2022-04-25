63cae10a7b9b599ba524df1615d1976098410240
tubakaradut <101792058+tubakaradut@users.noreply.github.com>
2022-04-25 19:53:08 +0300

    Add files via upload
    OpenClosedIlkesi

---
+++ OpenClosedP/OpenClosedP/DenizYoluSevk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedP
+{
+    public class DenizYoluSevk : Gonderi
+    {
+        public  int Id { get ; set ; }
+        public  string Ad { get ; set ; }
+        public double Agırlık { get ; set ; }
+
+        public double MaliyetHesapla()
+        {
+            return Agırlık * 2.0;
+        }
+    }
+}
---
+++ OpenClosedP/OpenClosedP/KarayoluSevki.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedP
+{
+    public class KarayoluSevki : Gonderi
+    {
+        public  int Id { get; set; }
+        public  string Ad { get; set; }
+        public  double Agırlık { get; set; }
+        public double MaliyetHesapla()
+        {
+            return Agırlık *1.5 ;
+        }
+    }
+}
